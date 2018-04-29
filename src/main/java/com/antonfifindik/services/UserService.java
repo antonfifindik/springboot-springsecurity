140213e59c8e270fb95863ef173c9b0e34313e33
Anton Fifindik <fifindik.av@gmail.com>
2018-04-29 14:15:36 +0300

    several changes

--- src/main/java/com/antonfifindik/services/UserService.java
+++ src/main/java/com/antonfifindik/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -27,13 +28,27 @@ public class UserService implements UserDetailsService {
 //            userDao.save(user);
 //        });
 
+
         if(!userDao.findByUsername("antonfifindik").isPresent()) {
             List<Role> userRoles = new ArrayList<>();
             userRoles.add(Role.USER);
             userRoles.add(Role.ADMIN);
             userDao.save(User.builder()
                     .username("antonfifindik")
-                    .password("123456")
+                    .password(new BCryptPasswordEncoder().encode("123456"))
+                    .authorities(userRoles)
+                    .accountNonExpired(true)
+                    .accountNonLocked(true)
+                    .credentialsNonExpired(true)
+                    .enabled(true).build());
+        }
+
+        if(!userDao.findByUsername("user1").isPresent()) {
+            List<Role> userRoles = new ArrayList<>();
+            userRoles.add(Role.USER);
+            userDao.save(User.builder()
+                    .username("user1")
+                    .password(new BCryptPasswordEncoder().encode("88888888"))
                     .authorities(userRoles)
                     .accountNonExpired(true)
                     .accountNonLocked(true)
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,9 +1,19 @@
 <html>
 <head>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        .mrg {
+            margin: 20px;
+        }
+    </style>
 </head>
 <body>
-<h1>Hello World!</h1>
-<a href="/logout" class="btn-warning btn-lg">Log out</a>
+<div class="mrg">
+    <h1 class="text-info">Hello World</h1>
+    <div class="alert alert-success" role="alert">
+        Вы успешно зашли! Токен: "${_csrf.token}"
+    </div>
+    <a href="/logout" class="btn-warning btn-lg">Log out</a>
+</div>
 </body>
 </html>
