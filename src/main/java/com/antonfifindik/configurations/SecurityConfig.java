575c6df6a6c7f65772ddcb4098ea9ecc1dfba7cb
Anton Fifindik <fifindik.av@gmail.com>
2018-04-29 12:13:49 +0300

    fixed problem with access after logout

--- src/main/java/com/antonfifindik/configurations/SecurityConfig.java
+++ src/main/java/com/antonfifindik/configurations/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity //отключение конфига security "из коробки"
@@ -23,7 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                 .and()
-                .logout().permitAll();
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").deleteCookies("remove")
+                .invalidateHttpSession(true).permitAll();
     }
 
     @Autowired
--- src/main/java/com/antonfifindik/controllers/MainController.java
+++ src/main/java/com/antonfifindik/controllers/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MainController {
     @RequestMapping("/")
